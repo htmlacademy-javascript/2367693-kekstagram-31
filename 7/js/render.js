1dc3f972d5ed455c3a314ccbdb233e05ed762b36
Keks <keks@htmlacademy.ru>
2024-03-17 19:29:29 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/render.js
@@ -0,0 +1,24 @@
+//находим шаблон
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+//создает одну миниатюру
+const createThumbnail = ({id, url, description, likes, comments}) => {
+  const pictureTemplateElement = pictureTemplate.cloneNode(true);
+  pictureTemplateElement.querySelector('.picture__img').src = url;
+  pictureTemplateElement.querySelector('.picture__img').alt = description;
+  pictureTemplateElement.querySelector('.picture__likes').textContent = likes;
+  pictureTemplateElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureTemplateElement.dataset.thumbnailId = id;
+
+  return pictureTemplateElement;
+};
+
+const renderThumbnails = (pictures, container) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+  container.appendChild(fragment);
+};
+
+export {renderThumbnails};

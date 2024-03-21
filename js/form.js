afb70baccd7b29598a12fb6964d56fea25d54d2a
Ivan Makrushin <ecaofc@gmail.com>
2024-03-21 22:03:04 +0700

    форма редактирования открывается, закрывается, очищается

--- js/form.js
+++ js/form.js
@@ -1,15 +1,40 @@
-const uploadInput = document.querySelector('.img-upload__input');
-const imgEditir = document.querySelector('.img-upload__overlay');
+const uploadInputElement = document.querySelector('.img-upload__input');
+const imgEditirElement = document.querySelector('.img-upload__overlay');
 const bodyElement = document.querySelector('body');
+const closeImgEditorElement = imgEditirElement.querySelector('.img-upload__cancel');
 
+//откроет редактор изображения если загружен файл
 const willOpenImgEditot = (evt) => {
   const file = evt.target.files[0];
   if (file) {
-    imgEditir.classList.remove('hidden');
+    imgEditirElement.classList.remove('hidden');
     bodyElement.classList.add('modal-open');
+    document.addEventListener('keydown', onImgEditortKeydown);
   }
 };
 
+//закрывает окно с редактирования, убирает обработчк на "escape"
+const hideImgEditor = () => {
+  imgEditirElement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onImgEditortKeydown);
+  uploadInputElement.reset();
+};
 
-uploadInput.addEventListener('change', willOpenImgEditot);
+//закроет окно, если нажат крестик
+const onCloseImgEditorButtonClick = () => {
+  hideImgEditor();
+};
+
+//закрое окно, если нажат "escape"
+function onImgEditortKeydown (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    hideImgEditor();
+  }
+}
+
+
+closeImgEditorElement.addEventListener('click', onCloseImgEditorButtonClick);
+uploadInputElement.addEventListener('change', willOpenImgEditot);
 

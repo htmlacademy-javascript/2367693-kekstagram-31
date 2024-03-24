6e4767cd8bf27d0693c94e2c2705b2c5cf18b8a8
Ivan Makrushin <ecaofc@gmail.com>
2024-03-24 17:07:57 +0700

    добавил валидацию при вводе и отправке

--- js/constants.js
+++ js/constants.js
@@ -72,11 +72,22 @@ const LIST_OF_COMMENTS = [
 
 const COMMENTS_COUNT_SHOW = 5;
 
+const MAX_HASHTAG_COUNT = 5;
+
+const VALID_SIMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+
+const ErrorText = {
+  INVALID_COUNT: `Максимум ${MAX_HASHTAG_COUNT} хэштегов`,
+  NOT_UNIQUE: 'Хэштеги должены быть уникальными',
+  INVALID_PATTERN: 'Неправильнай хэштег'
+};
+
 export {
   PHOTO_ID, URL_NUMS,
   COMMENT_ID, AVATAR_NUMBER,
   NUMBER_OF_LIKES, NUMBER_OF_COMMENTS,
   NUMBER_OF_THUMBNAILS, LIST_OF_NAMES,
   PHOTO_DESCRIPTION_LIST, LIST_OF_COMMENTS,
-  COMMENTS_COUNT_SHOW
+  COMMENTS_COUNT_SHOW, MAX_HASHTAG_COUNT,
+  VALID_SIMBOLS, ErrorText
 };
